34c50f18de870c0cbbca56c2640a12ccd923c4cd
Janice <janiceralecha@gmail.com>
2022-10-30 20:48:02 -0500

    commit tree 25e8916c272f83740f3cdb05665e4a2593dc09e2

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,27 @@
 class Person
+    def name=(personsName)
+        @name = personsName 
+    end 
+
+    def name 
+        @name 
+    end 
+
+    def job=(personsJob)
+        @job = personsJob 
+    end 
+
+    def job 
+        @job 
+    end
 
 end
+
+momo = Person.new 
+momo.name = "MoMo"
+momo.job = "Tech"
+
+puts momo.name
+#=> MoMo 
+puts momo.job
+#=> Tech
